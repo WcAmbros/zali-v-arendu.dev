26e38ba7d8fa05c8a823ecf29d17adf6958d9b88
WcAmbros <zahs88@yandex.ru>
2015-05-06 14:32:02 +0400

    Добавление поддержки  разных языков

--- common/config/main.php
+++ common/config/main.php
@@ -1,10 +1,20 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'language' => 'ru',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-
+        'i18n' => [
+            'translations' => [
+                'app*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'fileMap' => [
+                        'app' => 'app.php',
+                    ],
+                ],
+            ],
+        ],
     ],
 ];
---
+++ messages/ru/app.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sz
+ * Date: 06.05.2015
+ * Time: 13:16
+ */

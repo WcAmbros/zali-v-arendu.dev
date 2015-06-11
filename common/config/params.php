e0cb3b0e5a1d3f76e3ffdbe892c06d4b2350e471
WcAmbros <zahs88@yandex.ru>
2015-06-11 19:38:25 +0400

    Insert into params paths to backend and frontend directories

--- common/config/params.php
+++ common/config/params.php
@@ -3,4 +3,9 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
+    'name'=>'Залы в аренду',
+
+    'frontend'=>'http://zali-v-arendu.dev',
+    'backendPath' => dirname(dirname(__DIR__)) . '/backend',
+    'frontendPath' => dirname(dirname(__DIR__)) . '/frontend',
 ];
